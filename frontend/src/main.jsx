06af5dc88a71eecee58f82526506d4f328117f0e
Kristian Rusnak <rusnak71@uniba.sk>
2025-11-08 10:39:53 +0100

    Add initial routing and layout components for the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./App.jsx";
+import App from "./App";
 
 // we use react-redux as a single source of truth for our global store
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 );
