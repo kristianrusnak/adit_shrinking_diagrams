18b9fa1a92c1c45903566e9cc51d9b77652cf7b3
Kristian Rusnak <rusnak71@uniba.sk>
2025-11-10 16:33:01 +0100

    Refactor styles and structure for App and AppPage components

--- frontend/src/pages/AppPage/AppPage.tsx
+++ frontend/src/pages/AppPage/AppPage.tsx
@@ -4,26 +4,19 @@ import FileUploadButton from "../../components/ui/FileUploadButton";
 import FilePreview from "../../components/ui/FilePreview";
 import MessageInput from "../../components/ui/MessageInput";
 import DummyResponseComponent from "../../components/ui/DummyResponseComponent";
-import './AppPage.module.css';
+import styles from "./AppPage.module.css";
 
 export default function AppPage() {
   return (
-    <>
+    <div className={styles.page}>
       <ErrorProvider>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            maxWidth: "600px",
-          }}
-        >
+        <Box className={styles.content}>
           <FileUploadButton />
           <FilePreview />
           <MessageInput />
           <DummyResponseComponent />
         </Box>
       </ErrorProvider>
-    </>
+    </div>
   )
 }
