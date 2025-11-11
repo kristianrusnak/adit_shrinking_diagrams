b8a132718453e4c6729c6c7eb67c3c5865876abf
Kristian Rusnak <rusnak71@uniba.sk>
2025-11-11 16:22:35 +0100

    Add AboutPage and IntroNavbar components, implement BlurText for animated text display, and enhance styles

---
+++ frontend/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,10 @@
+import IntroNavbar from "../../components/layout/IntroNavbar/IntroNavbar";
+import styles from './AboutPage.module.css';
+
+export default function AboutPage() {
+  return (
+    <>
+      <IntroNavbar />
+    </>
+  );
+}
--- frontend/src/pages/IntroPage/IntroPage.tsx
+++ frontend/src/pages/IntroPage/IntroPage.tsx
@@ -1,50 +1,63 @@
 import styles from './IntroPage.module.css';
 import PixelBlast from './PixelBlast';
-import TextType from './TextType';
+import BlurText from "./BlurText";
+import IntroNavbar from '../../components/layout/IntroNavbar/IntroNavbar'
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
 
 export default function IntroPage() {
-    return (
-      <>
-        <div style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-          zIndex: -1
-        }}>
-          <PixelBlast
-            variant="circle"
-            pixelSize={6}
-            color="#B19EEF"
-            patternScale={3}
-            patternDensity={1.2}
-            pixelSizeJitter={0.5}
-            enableRipples
-            rippleSpeed={0.4}
-            rippleThickness={0.12}
-            rippleIntensityScale={1.5}
-            liquid={false}
-            liquidStrength={0.12}
-            liquidRadius={1.2}
-            liquidWobbleSpeed={5}
-            speed={0.6}
-            edgeFade={0.25}
-            transparent={false}
-          />
-        </div>
-        <div className={styles.changing_header_container}>
-          <TextType
-            text={["Shrinking UML diagrams", "Smaller is Better!", "Developed by OK team"]}
-            typingSpeed={70}
-            pauseDuration={5000}
-            deletingSpeed={40}
-            showCursor={true}
-            cursorCharacter="_"
-            className={styles.changing_header}
-          />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+        zIndex: -1
+      }}>
+        <PixelBlast
+          variant="circle"
+          pixelSize={11}
+          color="#b50202"
+          patternScale={3}
+          patternDensity={1.2}
+          pixelSizeJitter={0.5}
+          enableRipples
+          rippleSpeed={0.4}
+          rippleThickness={0.12}
+          rippleIntensityScale={1.5}
+          liquid={false}
+          liquidStrength={0.12}
+          liquidRadius={1.2}
+          liquidWobbleSpeed={5}
+          speed={0.6}
+          edgeFade={0.25}
+          transparent={false}
+        />
+      </div>
+
+      <IntroNavbar />
+
+      <BlurText
+        text="Smaller is Better — Shrinking Diagrams"
+        delay={300}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className={styles.changing_header_container}
+      />
+      <BlurText
+        text="Turn oversized UML into AI-ready insight. Our app prunes unimportant parts of your models so proprietary LLMs can reason on them—fast, cheap, and within context limits."
+        delay={80}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className={styles.changing_description_container}
+      />
+    </>
+  );
 }
