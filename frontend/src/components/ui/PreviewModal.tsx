1e1d8dad726efbf732dcf2c154f0e8b887b75a35
markooda <markooda@proton.me>
2026-03-12 20:37:00 +0100

    feat: implement diagram zooming/panning inside of preview component, added code highlighting

--- frontend/src/components/ui/PreviewModal.tsx
+++ frontend/src/components/ui/PreviewModal.tsx
@@ -13,6 +13,14 @@ import ImageIcon from "@mui/icons-material/Image";
 import CloseIcon from "@mui/icons-material/Close";
 import DownloadIcon from "@mui/icons-material/Download";
 import AssignmentIcon from "@mui/icons-material/Assignment";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+// setup code highlighting
+import Prism from "prismjs";
+import "prismjs/components/prism-plant-uml";
+import "prismjs/themes/prism.css";
+// import "prismjs/themes/prism-okaidia.css";
+
 interface PreviewModalProps {
   url: string;
   puml: string;
@@ -27,6 +35,7 @@ export const PreviewModal = ({
   handleSnackbarOpen,
 }: PreviewModalProps) => {
   const [mode, setMode] = useState<string>("left");
+  const [isPanning, setIsPanning] = useState(false);
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
@@ -124,29 +133,67 @@ export const PreviewModal = ({
         </IconButton>
       </Stack>
       {mode === "left" ? (
-        <img
-          src={url}
-          alt="PlantUML Diagram"
-          style={{
-            minWidth: "300px",
-            maxWidth: "90vw",
-            height: "auto",
-            maxHeight: "70vh",
-            objectFit: "contain",
-            display: "block",
-            zIndex: 0,
+        <Box
+          sx={{
+            width: "90vw",
+            height: "70vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden",
           }}
-        />
+        >
+          <TransformWrapper
+            initialScale={1}
+            minScale={0.5}
+            maxScale={4}
+            wheel={{ step: 0.15 }}
+            centerOnInit
+            onPanningStart={() => setIsPanning(true)}
+            onPanningStop={() => setIsPanning(false)}
+          >
+            <TransformComponent
+              wrapperStyle={{
+                width: "100%",
+                height: "100%",
+                cursor: isPanning ? "grabbing" : "grab",
+              }}
+              contentStyle={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                component="img"
+                src={url}
+                alt="PlantUML Diagram"
+                sx={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  width: "auto",
+                  height: "auto",
+                  display: "block",
+                  userSelect: "none",
+                  pointerEvents: "none",
+                }}
+              />
+            </TransformComponent>
+          </TransformWrapper>
+        </Box>
       ) : (
         <Box
+          component="pre"
           sx={{
             flexGrow: 1,
             minWidth: "500px",
-            height: "auto",
+            m: 0,
+            p: 2,
             maxHeight: "70vh",
             overflow: "auto",
             borderRadius: 1,
-            p: 1,
             backgroundColor: grey[100],
             fontFamily: "monospace",
             whiteSpace: "pre-wrap",
@@ -163,14 +210,22 @@ export const PreviewModal = ({
             },
           }}
         >
-          <Typography
-            variant="body2"
+          <Box
+            component="code"
+            className="language-plant-uml"
             sx={{
+              fontFamily: "inherit",
+              fontSize: "0.9rem",
               color: "black",
             }}
-          >
-            {puml}
-          </Typography>
+            dangerouslySetInnerHTML={{
+              __html: Prism.highlight(
+                puml,
+                Prism.languages["plant-uml"],
+                "plant-uml",
+              ),
+            }}
+          />
         </Box>
       )}
     </Box>
